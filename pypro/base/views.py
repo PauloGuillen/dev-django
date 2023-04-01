c5bbeffcb45b6f1c4e18057358b76bc070cc1835
PauloGuillen <psm.guillen@gmail.com>
2023-04-01 09:32:56 -0300

    pagina inicial html

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,7 +1,6 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
 def home(request):
-    raise ValueError
-    return HttpResponse('<HTML><BODY>olá Django</BODY></HTML>', content_type='text/html')
+    return render(request, 'base/home.html')
