7d50353ce908d09d79e88180005d7c04cbe1740b
PauloGuillen <psm.guillen@gmail.com>
2023-04-07 10:12:33 -0300

    criado o app aperitivos

--- pypro/aperitivos/apps.py
+++ pypro/aperitivos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AperitivosConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "aperitivos"
+    name = "pypro.aperitivos"
