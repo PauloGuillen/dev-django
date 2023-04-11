294a41a59fab95018cc2dcc3786a079622f64f8e
PauloGuillen <psm.guillen@gmail.com>
2023-04-10 21:57:15 -0300

    HTTP e templates close #50

---
+++ pypro/aperitivos/tests/test_indice.py
@@ -0,0 +1,35 @@
+from django.urls import reverse
+import pytest
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+        'titulo',
+        [
+            'Video Aperitivo: Motivação',
+            'Instalação windows'
+        ]
+)
+def test_titulo_video(resp, titulo):
+    assert_contains(resp, titulo)
+
+
+@pytest.mark.parametrize(
+        'slug',
+        [
+            'motivacao',
+            'instalacao-windows'
+        ]
+)
+def test_link_video(resp, slug):
+    link_video = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{ link_video }"')
